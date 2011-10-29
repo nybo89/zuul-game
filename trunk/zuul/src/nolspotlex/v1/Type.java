5081b10e18121a190e833438f4781f12aaa208bd
boursier.alexandre@gmail.com <boursier.alexandre@gmail.com@f5f79e94-2c69-1785-1bfb-446730197141>
2011-10-29 12:14:46 +0000

    Correction goRandomRoom

--- trunk/zuul/src/nolspotlex/v1/Type.java
+++ trunk/zuul/src/nolspotlex/v1/Type.java
@@ -10,5 +10,5 @@ package nolspotlex.v1;
 public enum Type {
     // One of the first SIX ELEMENTS is randomly chosen to be a trap door
     RECEPTION, WAITING_ROOM, CORRIDOR, PARKING, RESTAURANT, INTENSE_CARE_ROOM,
-    DELIVERY_ROOM, TOILETS, OUTSIDE, BEDROOM, TRAP_ROOM, WAY_TO_GO;
+    DELIVERY_ROOM, BEDROOM, TOILETS, OUTSIDE, TRAP_ROOM, WAY_TO_GO;
 }
